866784a0462a67b36fe9194399bafc51b323bf00
bound1ess <bound1ess.dev@gmail.com>
2015-02-10 21:34:47 +0600

    Will it work?

--- src/segmentation/hashtag-and-url.rb
+++ src/segmentation/hashtag-and-url.rb
@@ -1,18 +1,32 @@
 words = File.read(File.dirname(__FILE__) + "/words.txt").split $/
 
-gets.to_i.times.each do
+gets.to_i.times do
     element, tokens = gets.strip, []
 
     # Check if it's a hashtag.
     if element[0] == "#"
-        element[/#/] = ""
+        element["#"] = ""
     else
         # Otherwise it'a an URL.
-        element[/www\./] = ""
+        element["www."] = ""
         element[/\.(\w+)/] = ""
     end
 
     # Logic
+    while element.length != 0
+        length = 0
+
+        element.length.times do
+            length += 1
+
+            if words.include? element[0, length]
+                tokens.push element[0, length]
+                element[element[0, length]] = ""
+
+                break
+            end
+        end
+    end
 
     puts tokens.join " "
 end
