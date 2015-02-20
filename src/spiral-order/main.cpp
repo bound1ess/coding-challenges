7a3357ebc13e408472e60a5aed4f94a7e2ee5c8a
bound1ess <bound1ess.dev@gmail.com>
2015-02-20 22:08:20 +0600

    Produce and print a grid.

--- src/spiral-order/main.cpp
+++ src/spiral-order/main.cpp
@@ -2,8 +2,8 @@
 #include <cstdlib>
 
 int** create_grid(int height, int width);
-void print_grid(int** grid);
-void print_in_spiral_order(int** grid);
+void print_grid(int** grid, int height, int width);
+void print_in_spiral_order(int** grid, int height, int width);
 
 int main(void)
 {
@@ -12,8 +12,38 @@ int main(void)
 
     int** grid = create_grid(height, width);
 
-    print_grid(grid);
-    print_in_spiral_order(grid);
+    print_grid(grid, height, width);
+    print_in_spiral_order(grid, height, width);
 
     return 0;
+}
+
+int** create_grid(int height, int width)
+{
+    int** grid = new int*[height];
+
+    for (int i = 0; i < height; i++) {
+        grid[i] = new int[width];
+
+        for (int j = 0; j < width; j++) {
+            grid[i][j] = rand() % 100;
+        }
+    }
+
+    return grid;
+}
+
+void print_grid(int** grid, int height, int width)
+{
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            std::cout << grid[i][j] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void print_in_spiral_order(int** grid, int heigth, int width)
+{
 }
